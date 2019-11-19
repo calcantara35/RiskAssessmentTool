b7baa29e5af36cbcfe8c51ea4cb7ae08e00f9f49
Cesar Alcantara <cja5391@psu.edu>
2019-11-19 13:20:01 -0500

    got rid of some comments

--- main.js
+++ main.js
@@ -3,41 +3,6 @@ let totalBusinessVar = 0;
 let totalOperationsVar = 0;
 let totalFinanicalAppVar = 0;
 
-// Need to convert the textbox values into number data types
-// need to make these global
-//   first questions
-// let question1Val = Number(document.querySelector("#question1").value);
-// let question2Val = Number(document.querySelector("#question2").value);
-// let question3Val = Number(document.querySelector("#question3").value);
-// let question4Val = Number(document.querySelector("#question4").value);
-// let question5Val = Number(document.querySelector("#question5").value);
-// let question6Val = Number(document.querySelector("#question6").value);
-// let question7Val = Number(document.querySelector("#question7").value);
-// let question8Val = Number(document.querySelector("#question8").value);
-// let question9Val = Number(document.querySelector("#question9").value);
-// console.log(question1Val);
-
-// //   second questions
-// let question10Val = Number(document.querySelector("#question10").value);
-// let question11Val = Number(document.querySelector("#question11").value);
-// let question12Val = Number(document.querySelector("#question12").value);
-// let question13Val = Number(document.querySelector("#question13").value);
-// let question14Val = Number(document.querySelector("#question14").value);
-// let question15Val = Number(document.querySelector("#question15").value);
-// let question16Val = Number(document.querySelector("#question16").value);
-// let question17Val = Number(document.querySelector("#question17").value);
-// let question18Val = Number(document.querySelector("#question18").value);
-//   third questions
-// let question19Val = Number(document.querySelector("#question19").value);
-// let question20Val = Number(document.querySelector("#question20").value);
-// let question21Val = Number(document.querySelector("#question21").value);
-// let question22Val = Number(document.querySelector("#question22").value);
-// let question23Val = Number(document.querySelector("#question23").value);
-// let question24Val = Number(document.querySelector("#question24").value);
-// let question25Val = Number(document.querySelector("#question25").value);
-// let question26Val = Number(document.querySelector("#question26").value);
-// let question27Val = Number(document.querySelector("#question27").value);
-
 // Event listener for business
 document.querySelector("#submitBtnBusiness").addEventListener("click", () => {
   let question1Val = Number(document.querySelector("#question1").value);
